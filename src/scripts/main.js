ae102f14f7494df4fe7b1987f40466448d9772d6
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2020-11-19 18:12:28 -0600

    Plants for sell now appear on the webpage in a grid that always has 6 plants to a row

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,7 @@ import { createPlan } from "./plan.js"
 import { plantSeeds } from "./tractor.js"
 import { usePlants } from "./field.js"
 import { harvestPlants } from "./harvester.js"
+import { catalog } from "./catalog.js"
 
 // Create randomly generated planting plans
 const plantingPlans = createPlan();
@@ -24,3 +25,5 @@ console.log(whatToPlant);
 const harvestArray = harvestPlants(whatToPlant);
 console.log("These are the plants we harvested")
 console.log(harvestArray)
+
+catalog();
