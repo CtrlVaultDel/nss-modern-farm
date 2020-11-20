ae77ac8c9e5c1998cdca84c03220fad13e7330c7
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2020-11-20 15:04:19 -0600

    Sorted the grid alphabetically

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -4,6 +4,20 @@ import { harvestPlants } from "./harvester.js"
 export const catalog = () =>{
     const grownPlants = usePlants();
     const plantsToSell = harvestPlants(grownPlants);
+
+    console.log("These are the plantsToSell");
+    console.log(plantsToSell);
+    plantsToSell.sort(function(a,b){
+        if (a.type < b.type){
+            return -1;
+        }
+        else if (a.type > b.type){
+            return 1;
+        }
+        return 0;
+    });
+    console.log(plantsToSell);
+
     const contentElement = document.querySelector(".container");
 
     plantsToSell.forEach(element => {
