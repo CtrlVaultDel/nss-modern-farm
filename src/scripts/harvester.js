e63a7a959e7fb252f68102489c76fc6a520501c5
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2020-12-01 12:08:26 -0600

    Converted forEach functions in catalog.js and harvester.js to use .map instead

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -2,18 +2,17 @@
 // A new array will be returned with 
 export const harvestPlants = (plantsArray) =>{
     const harvestedArray = [];
-    plantsArray.forEach(element => {
-        // If it is anything but corn, add the plantObject to harvestedArray (element.output number of times)
-        if(element.type !== "Corn"){
-            for(let i=0; i < element.output; i++){
-                harvestedArray.push(element);
+    plantsArray.map(plant => {
+        if(plant.type !== "Corn"){
+            for(let i=0; i < plant.output; i++){
+                harvestedArray.push(plant);
             }
         // If it is corn, only half of it is going to be sold
-        } else if (element.type === "Corn"){
-            for(let i=0; i < (element.output/2); i++){
-                harvestedArray.push(element);
+        } else if (plant.type === "Corn"){
+            for(let i=0; i < (plant.output/2); i++){
+                harvestedArray.push(plant);
             }
-        }
-    });
+        } 
+    })
     return harvestedArray;
 }
